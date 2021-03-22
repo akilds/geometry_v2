cd58a3b24951a21f6d73562e3d0cbad70766b01b
akilds <akilrainadesigan@gmail.com>
2021-03-22 15:30:27 +0530

    Comparing lengths

--- geo.java
+++ geo.java
@@ -5,15 +5,29 @@ public class geo
 {
  public static void main(String[] args)
  {
-  double x1, y1, x2, y2, length, b=2;
+  double x1, y1, x2, y2, length1, b=2;
+  double a1, b1, a2, b2, length2;
   Scanner sc = new Scanner(System.in);
+  System.out.println("Emter the coordines of line 1.");
   System.out.println("Enter coordinates (x1, y1) : ");
   x1=sc.nextDouble();
   y1=sc.nextDouble();
   System.out.println("Enter coordinates (x2, y2) : ");
   x2=sc.nextDouble();
   y2=sc.nextDouble();
-  length = Math.sqrt(Math.pow((x2-x1),b) + Math.pow((y2-y1),b));
-  System.out.println("The length of the line joining the coordinates : "+length);
+  length1 = Math.sqrt(Math.pow((x2-x1),b) + Math.pow((y2-y1),b));
+  String s1 = Double.toString(length1);
+  System.out.println("The length of the line joining the coordinates : " + length1);
+  System.out.println("Emter the coordines of line 2.");
+  System.out.println("Enter coordinates (a1, b1) : ");
+  a1=sc.nextDouble();
+  b1=sc.nextDouble();
+  System.out.println("Enter coordinates (a2, b2) : ");
+  a2=sc.nextDouble();
+  b2=sc.nextDouble();
+  length2 = Math.sqrt(Math.pow((a2-a1),b) + Math.pow((b2-b1),b));
+  String s2 = Double.toString(length2);
+  System.out.println("The length of the line joining the coordinates : " + length2);
+  System.out.println("The equality of lines : " + s1.equals(s2));
  }
 }
